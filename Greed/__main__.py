8f295fd80ed77e0d86a3659385b2ed6a9f795301
carterlan <carteronelah@gmail.com>
2022-02-17 18:02:51 -0700

    Keyboard change

--- Greed/__main__.py
+++ Greed/__main__.py
@@ -21,7 +21,7 @@ CELL_SIZE = 15
 FONT_SIZE = 15
 COLS = 60
 ROWS = 40
-CAPTION = "Robot Finds Kitten"
+CAPTION = "Rocks and Gems"
 DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
@@ -42,7 +42,7 @@ def main():
     
     # create the robot
     x = int(MAX_X / 2)
-    y = int(MAX_Y / 2)
+    y = int(585)
     position = Point(x, y)
 
     robot = Actor()
