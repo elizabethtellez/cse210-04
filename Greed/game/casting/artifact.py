89813bcef52981ed050e92f039b95741919fccb7
ederson192 <ederson192@gmail.com>
2022-02-19 17:38:15 -0500

    Working

--- Greed/game/casting/artifact.py
+++ Greed/game/casting/artifact.py
@@ -1,10 +1,13 @@
 from game.casting.actor import Actor
+from game.shared.point import Point
 
 
 class Artifact (Actor): 
     def __init__(self):
         super().__init__()
         self._message = ""
+        self._velocity = Point(0, 0)
+        self._position = Point(0, 0)
         
     def get_message(self):
         """Gets the artifact's message.
@@ -22,6 +25,33 @@ class Artifact (Actor):
         """
         self._message = message
 
+    def get_velocity(self):
+        """Gets the actor's speed and direction.
+        
+        Returns:
+            Point: The actor's speed and direction.
+        """
+        return self._velocity
+
+    def set_velocity(self, velocity):
+        """Updates the velocity to the given one.
+        
+        Args:
+            velocity (Point): The given velocity.
+        """
+        self._velocity = velocity
+
+    def falling(self):
+        x = self._position.get_x()
+        y = self._position.get_y() + 1
+        self._position = Point(x, y)
        
 
+    def set_position(self, position):
+        """Updates the position to the given one.
+        
+        Args:
+            position (Point): The given position.
+        """
+        self._position = position
 
