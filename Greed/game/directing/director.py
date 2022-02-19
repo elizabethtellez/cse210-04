319fea9d4a12791c753cc15fbedca905d84f1385
carterlan <carteronelah@gmail.com>
2022-02-19 16:20:39 -0700

    Revert "Working"
    This reverts commit 89813bcef52981ed050e92f039b95741919fccb7.

--- Greed/game/directing/director.py
+++ Greed/game/directing/director.py
@@ -1,8 +1,3 @@
-import random 
-from game.shared.color import Color
-from game.shared.point import Point
-from game.casting.artifact import Artifact
-
 class Director:
     """A person who directs the game. 
     
@@ -22,7 +17,6 @@ class Director:
         """
         self._keyboard_service = keyboard_service
         self._video_service = video_service
-        self._counter= 0
         
     def start_game(self, cast):
         """Starts the game using the given cast. Runs the main game loop.
@@ -44,17 +38,8 @@ class Director:
             cast (Cast): The cast of actors.
         """
         robot = cast.get_first_actor("robots")
-
-        artifacts = cast.get_actors("artifacts")
-
         velocity = self._keyboard_service.get_direction()
-
-        robot.set_velocity(velocity)  
-
-        for artifact in artifacts:
-            artifact.set_velocity(velocity)
-
-        # artifacts.set_velocity(velocity)    
+        robot.set_velocity(velocity)        
 
     def _do_updates(self, cast):
         """Updates the robot's position and resolves any collisions with artifacts.
@@ -70,44 +55,11 @@ class Director:
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
         robot.move_next(max_x, max_y)
-
         
         for artifact in artifacts:
-            artifact.falling()
             if robot.get_position().equals(artifact.get_position()):
                 message = artifact.get_message()
-                banner.set_text(message)
-
-
-        if self._counter == 10:
-            
-            test = ['42','9744']
-            text = chr(int(random.choice(test))) 
-            # * artificat , □ = 0x25A1
-
-            #message = messages[n]
-
-            x = random.randint(1, 40 - 1)
-            y = 1
-            position = Point(x, y)
-            position = position.scale(15)
-
-            r = random.randint(0, 255)
-            g = random.randint(0, 255)
-            b = random.randint(0, 255)
-            color = Color(r, g, b)
-            
-            artifact = Artifact()
-            artifact.set_text(text)
-            artifact.set_font_size(15)
-            artifact.set_color(color)
-            artifact.set_position(position)
-            # artifact.set_message(message)
-            cast.add_actor("artifacts", artifact)
-            
-            self._counter=0
-        else:
-            self._counter +=1
+                banner.set_text(message)    
         
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
